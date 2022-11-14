b1ce400e4d460c640800cd2fb72048acd9beb174
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-15 00:31:26 +0600

    testimonial header image updated

--- src/Pages/Home/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -3,6 +3,7 @@ import Testimoinal from "./Testimoinal";
 import people1 from "../../../assets/images/people1.png";
 import people2 from "../../../assets/images/people2.png";
 import people3 from "../../../assets/images/people3.png";
+import quote from "../../../assets/icons/quote.svg";
 
 const Testimonials = () => {
   const testimonialData = [
@@ -33,8 +34,15 @@ const Testimonials = () => {
   ];
   return (
     <section className="mt-28">
-      <h1 className="text-xl text-bold text-primary mb-3">Testimonial</h1>
-      <h2 className="text-5xl text-semibold text-white">What Our Patients Says</h2>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-xl text-bold text-primary mb-3">Testimonial</h1>
+          <h2 className="text-5xl text-semibold text-white">What Our Patients Says</h2>
+        </div>
+        <div className="w-1/6">
+          <img src={quote} alt="quote" className="w-100" />
+        </div>
+      </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-14">
         {testimonialData.map((testimonial) => (
           <Testimoinal key={testimonial.id} testimonial={testimonial}></Testimoinal>
