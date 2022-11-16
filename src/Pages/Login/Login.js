de054241d0e14b59a937a6c9769b1d9b159627f8
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-16 21:52:11 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
-      <p></p>
+    <div className="flex justify-center items-center">
+      <div className="w-[385px] h-[800px] p-6">
+        <h1 className="text-xl font-semibold text-center">Login</h1>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" {...register("email", { required: "Email Address is required" })} className="input input-bordered w-full" />
+            {errors.email && (
+              <p className="text-red-600 mt-1" role="alert">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password field is required",
+                minLength: { value: 6, message: "Password must be 6 character or longer!" },
+              })}
+              className="input input-bordered w-full"
+            />
+            {errors.password && (
+              <p role="alert" className="text-red-600 mt-1">
+                {errors.password?.message}
+              </p>
+            )}
+            <label className="label">
+              <span className="label-text">Forgot password?</span>
+            </label>
+          </div>
+          <div className="form-control w-full">
+            <input type="submit" value="LOGIN" className="btn btn-accent w-full" />
+          </div>
+        </form>
+        <div className="flex flex-col w-full border-opacity-50">
+          <p className="mt-1">
+            New to Doctors Portal?
+            <Link to="/signup" className="text-secondary">
+              Create new account
+            </Link>
+          </p>
+          <div className="divider">OR</div>
+          <button className="btn btn-outline">CONTINUE WITH GOOGLE</button>
+        </div>
+      </div>
     </div>
   );
 };
